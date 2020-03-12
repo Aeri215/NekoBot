29162a9045d4490ac97d2e7117fbf6b64bd669ee
ThibaudHardy <thibaudhardy1@gmail.com>
2020-03-12 09:46:09 +0100

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 git reset --hard HEAD
 git pull
-docker rmi nekobot --force
+docker stop $(docker ps -a -q)
+docker container prune --force
+docker rmi nakobot --force
 docker build -t nekobot .
 docker run -d nekobot
