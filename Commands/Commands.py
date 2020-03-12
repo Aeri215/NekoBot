fa39a3c5c5e04cad9e082361185da40bf077cafc
ThibaudHardy <thibaudhardy1@gmail.com>
2020-03-12 10:20:08 +0100

    Test

--- Commands/Commands.py
+++ Commands/Commands.py
@@ -248,7 +248,7 @@ class Commands(object):
 
     @staticmethod
     async def ping(self, message, cmd):
-        response = discord.Embed(title="Pong3")
+        response = discord.Embed(title="Pong4")
         return response
 
     @staticmethod
